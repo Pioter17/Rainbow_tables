44368a623b4bddc7677932280cabfb77ef210223
Pioter17 <piotrwisnie@wp.pl>
2024-06-05 21:03:01 +0200

    some progress

--- src/main/java/prir/RainbowTableDES.java
+++ src/main/java/prir/RainbowTableDES.java
@@ -8,20 +8,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class RainbowTableDES {
 
     private static final String ALGORITHM = "DES";
-    private static final int TABLE_SIZE = 6; // Increased size
-    private static final int CHAIN_LENGTH = 10; // Length of the reduction chain
-    private static final int THREAD_COUNT = 1;
+    private static final int TABLE_SIZE = 2000; // Increased size for better coverage
+    private static final int CHAIN_LENGTH = 10000; // Increased chain length
+    private static final int THREAD_COUNT = 4; // Increased number of threads
     private final ConcurrentHashMap<String, String> rainbowTable = new ConcurrentHashMap<>();
     private final Random random = new Random();
+    private static final SecretKey key;
+
+    static {
+        try {
+            key = generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         RainbowTableDES rainbowTableDES = new RainbowTableDES();
@@ -31,30 +47,50 @@ public class RainbowTableDES {
         System.out.println("Time taken: " + (endTime - startTime) + " ms");
 
         // Example of usage
-        String plainText = "ab";
+        String plainText = "abceh";
         String cipherText = rainbowTableDES.encrypt(plainText);
-        System.out.println("CipherText: " + cipherText);
 
+        startTime = System.currentTimeMillis();
         String decryptedText = rainbowTableDES.lookup(cipherText);
-        System.out.println("Decrypted Text: " + decryptedText);
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endTime - startTime) + " ms");
+
+        System.out.println("CipherText: " + cipherText);
+        if (decryptedText == null) {
+            System.out.println("Nie znaleziono hasła!");
+        } else {
+            System.out.println("Decrypted Text: " + decryptedText);
+        }
     }
 
     public void generateRainbowTable(String rainbowTableFile, String passwordsFile) throws Exception {
+
+        HashSet<String> set = new HashSet<>();
+        do {
+            String startPlainText = generateRandomPassword();
+            set.add(startPlainText);
+        } while (set.size() != TABLE_SIZE);
+        System.out.println("Wyszedłem z while");
+
+        List<String> firstColumn = set.stream().toList();
+
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
 
         try (BufferedWriter rainbowWriter = new BufferedWriter(new FileWriter(rainbowTableFile));
              BufferedWriter passwordWriter = new BufferedWriter(new FileWriter(passwordsFile))) {
 
             for (int i = 0; i < TABLE_SIZE; i++) {
+                int finalI = i;
                 executor.submit(() -> {
                     try {
-                        String startPlainText = generateRandomPassword();
+                        String startPlainText = firstColumn.get(finalI);
                         String endPlainText = startPlainText;
                         String cipherText = null;
                         StringBuilder chain = new StringBuilder(startPlainText);
 
                         for (int j = 0; j < CHAIN_LENGTH; j++) {
                             cipherText = encrypt(endPlainText);
+                            chain.append(" -> ").append(cipherText);
                             endPlainText = reduce(cipherText, j);
                             chain.append(" -> ").append(endPlainText);
                             // Write each generated password to the file
@@ -65,7 +101,7 @@ public class RainbowTableDES {
                         synchronized (rainbowWriter) {
                             rainbowWriter.write(chain.toString() + "\n");
                         }
-                        rainbowTable.put(endPlainText, startPlainText);
+                        rainbowTable.put(startPlainText, endPlainText);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -80,8 +116,8 @@ public class RainbowTableDES {
     }
 
     private String generateRandomPassword() {
-        int length = 2; // length of the password
-        String characters = "abc";
+        int length = 5; // length of the password
+        String characters = "abcdefghijklmnoprstuvwxyz";
         StringBuilder password = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
             password.append(characters.charAt(random.nextInt(characters.length())));
@@ -90,7 +126,7 @@ public class RainbowTableDES {
     }
 
     public String encrypt(String plainText) throws Exception {
-        SecretKey key = generateKey();
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] encrypted = cipher.doFinal(plainText.getBytes());
@@ -108,10 +144,49 @@ public class RainbowTableDES {
     }
 
     public String lookup(String cipherText) {
-        return rainbowTable.get(cipherText);
+        for (int i = CHAIN_LENGTH - 1; i >= 0; i--) {
+            final String[] currentText = {cipherText, "", "", ""};
+            for (int j = i; j < CHAIN_LENGTH; j++) {
+                currentText[0] = reduce(currentText[0], j);
+                if (j == CHAIN_LENGTH - 1 && rainbowTable.containsValue(currentText[0])) {
+                    int finalI = i;
+                    rainbowTable.forEach((k, v) -> {
+                        if (rainbowTable.get(k).equals(v)) {
+                            currentText[0] = k;
+                            for (int z = 0; z < finalI; z++) {
+                                try {
+                                    currentText[0] = encrypt(currentText[0]);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+//                                System.out.println("hash before " + currentText[0]);
+                                currentText[0] = reduce(currentText[0], z);
+                                try {
+                                    currentText[1] = encrypt(currentText[0]);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                                currentText[2] = reduce(currentText[1], z+1);
+                                currentText[3] = reduce(cipherText, z+1);
+                            }
+                        }
+                    });
+                    String result = currentText[0];
+//                    System.out.println("tekst wynik: " + result + ", hash wyniku: " + currentText[1] + ", redukcja hasha wyniku: " + currentText[2] + ", redukcja hasha hasła: " + currentText[3]);
+                    return result;
+//                    return rainbowTable.get(currentText);
+                }
+                try {
+                    currentText[0] = encrypt(currentText[0]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 
-    private SecretKey generateKey() throws NoSuchAlgorithmException {
+    private static SecretKey generateKey() throws NoSuchAlgorithmException {
         KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
         keyGenerator.init(new SecureRandom());
         return keyGenerator.generateKey();
